acb2ea8b8e1dee507186c8a2797171ef49674d31
Babatunde Alli <tunsmart@gmail.com>
2015-07-30 14:32:07 +0100

    finshed CRUD for Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post, :notice => 'Post was created successfully.'
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "Post updated"
+      redirect_to @post
+
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to(root_url)
+   end
+
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :body)
+end
+
+end
---
+++ db/migrate/20150728020636_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.boolean :published
+
+      t.timestamps null: false
+    end
+    add_index :posts, :title
+  end
+end
